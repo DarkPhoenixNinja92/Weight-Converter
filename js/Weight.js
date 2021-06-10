d484217c91d3b37f8f261d58044c69f71e574955
Shaun Pour <scpour@gmail.com>
2021-06-10 13:02:09 -0400

    2

--- js/Weight.js
+++ js/Weight.js
@@ -1,6 +1,10 @@
-function convertToKilo() {
+let convertToKilo = () => {
     let weightVal = parseInt(document.getElementById("initial-weight").value);
     let weightConvert = (weightVal / 2) - (1 / 10);
     document.getElementById("kg-weight").innerHTML = "Your weight in kilograms is ";
     document.getElementById("kg-weight").append(weightConvert);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("submit-weight").addEventListener('click', convertToKilo);
+})
