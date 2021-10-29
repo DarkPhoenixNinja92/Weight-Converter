3663288d9c7f734f60cbb79bbd83791c968c597e
Shaun Pour <scpour@gmail.com>
2021-10-29 13:44:00 -0400

    2.02

--- js/Weight.js
+++ js/Weight.js
@@ -1,8 +1,24 @@
+let weightType;
+let weightVal = parseInt(document.getElementById("initial-weight")).value;
+let poundBtn = document.querySelector(".pounds");
+let kiloBtn = document.querySelector(".kilos");
+let weight = document.querySelector(".weight");
+
 let convertToKilo = () => {
-    let weightVal = parseInt(document.getElementById("initial-weight").value);
-    let weightConvert = (weightVal / 2) - (1 / 10);
+    let kiloConvert = (weightVal / 2) - (1 / 10);
     document.getElementById("kg-weight").innerHTML = "Your weight in kilograms is ";
-    document.getElementById("kg-weight").append(weightConvert);
+    document.getElementById("kg-weight").append(kiloConvert);
+}
+
+let convertToPounds = () => {
+    let poundConvert = (weightVal * 2) - (1 / 10);
+    document.getElementById("kg-weight").innerHTML = "Your weight in kilograms is ";
+    document.getElementById("kg-weight").append(poundConvert);
+}
+
+function unitSelection() {
+    weightType = weight.textContent;
+    console.log(weightType);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
